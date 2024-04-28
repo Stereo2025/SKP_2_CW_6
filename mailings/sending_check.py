77909ba32783f2f682e207280c5ea5dce7139460
Stereo2025 <teneloks@gmail.com>
2024-04-28 08:22:16 +0300

    cronie

--- mailings/sending_check.py
+++ mailings/sending_check.py
@@ -14,7 +14,7 @@ def start_mailing():
     mailing_list = Mailing.objects.filter(date__lte=now.date(),
                                           time__gte=lower_bound_time.time(),
                                           time__lte=upper_bound_time.time(),
-                                          status='created')
+                                          status__in=['created', 'started'])
     for mailing in mailing_list:
         user = mailing.user
         mailing.status = 'started'
@@ -49,3 +49,56 @@ def start_mailing():
         )
         log.save()
         mailing.save()
+
+# def start_mailing():
+#     """Функция проверки и начала рассылок."""
+#     now = datetime.now()
+#     current_datetime = datetime.combine(now.date(), now.time())
+#     lower_bound_time = current_datetime - timedelta(seconds=30)
+#     upper_bound_time = current_datetime + timedelta(seconds=30)
+#
+#     mailing_list = Mailing.objects.filter(date__lte=now.date(),
+#                                           time__gte=lower_bound_time.time(),
+#                                           time__lte=upper_bound_time.time(),
+#                                           status='created')
+#     for mailing in mailing_list:
+#         user = mailing.user
+#         mailing.status = 'pending'
+#         mailing.save()
+#
+#         clients = mailing.client.all()
+#         message = mailing.message
+#         response = None
+#         try:
+#             response = send_mailing(clients, message)
+#             mailing.status = 'completed'
+#         except smtplib.SMTPException as e:
+#             response = False
+#             log_message = str(e)
+#             mailing.status = 'failed'
+#
+#         mailing.save()
+#
+#         if mailing.periodicity:
+#             update_mailing_date(mailing, now)
+#
+#         log = Log.objects.create(
+#             time=now,
+#             status=response,
+#             server_response=log_message if not response else '',
+#             mailing=mailing,
+#             user=user
+#         )
+#         log.save()
+#
+#
+# def update_mailing_date(mailing, now):
+#     """Функция обновляет дату следующей рассылки в зависимости от периодичности."""
+#     if mailing.periodicity == 'day':
+#         mailing.date += timedelta(days=1)
+#     elif mailing.periodicity == 'week':
+#         mailing.date += timedelta(weeks=1)
+#     elif mailing.periodicity == 'month':
+#         days_in_month = monthrange(now.year, now.month)[1]
+#         mailing.date += timedelta(days=days_in_month)
+#     mailing.save()
